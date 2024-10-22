e9f016c00bbee0c459cce9fd09975f9447b61a36
Diego Ochoa <daochoa@pucp.edu.pe>
2024-10-21 23:11:40 -0500

    Add: Oficinas

--- src/components/Mapa/Mapa.tsx
+++ src/components/Mapa/Mapa.tsx
@@ -1,17 +1,29 @@
 import { APIProvider, ColorScheme, Map } from '@vis.gl/react-google-maps';
 import React from 'react';
+import styles from './Mapa.module.css';
+import oficinas from '../../data/oficinas';
+import OficinaMarker from '../Markers/OficinaMarker/OficinaMarker';
 
 const Mapa: React.FC = () => {
   return (
     <APIProvider apiKey="AIzaSyAf4vRvjVvt-AuStWjrfbA-tJNYouHBpb4">
       <Map
-        style={{ width: '100%', height: '100%' }}
+        className={styles.mapa}
         defaultCenter={{ lat: -12.066435, lng: -77.044072 }}
         defaultZoom={15}
         gestureHandling={'greedy'}
         disableDefaultUI={true}
         colorScheme={ColorScheme.LIGHT}
-      />
+        mapId={"49ae42fed52588c3"}
+        mapTypeId={"roadmap"}
+      >
+        {oficinas.map((oficina, index) => (
+          <OficinaMarker
+            key={index}
+            oficina={oficina}
+          />
+        ))}
+      </Map>
     </APIProvider>
   );
 };
---
+++ src/components/Markers/OficinaMarker/OficinaMarker.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { AdvancedMarker, AdvancedMarkerProps } from '@vis.gl/react-google-maps';
+import { Store } from '@mui/icons-material';
+
+type Oficina = {
+  ubigeo: string;
+  departamento: string;
+  provincia: string;
+  latitud: number;
+  longitud: number;
+  regionNatural: string;
+  almacen: number;
+};
+
+interface OficinaMarkerProps extends Omit<AdvancedMarkerProps, 'position'> {
+  oficina: Oficina;
+}
+
+const OficinaMarker: React.FC<OficinaMarkerProps> = ({ oficina, ...markerProps }) => {
+  return (
+    <AdvancedMarker
+      position={{ lat: oficina.latitud, lng: oficina.longitud }}
+      {...markerProps}
+    >
+      <Store />
+    </AdvancedMarker>
+  );
+};
+
+export default OficinaMarker;
