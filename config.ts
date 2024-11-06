11179b35d3b36cca56661b2c8eeec8563764028b
Diego Ochoa <daochoa@pucp.edu.pe>
2024-11-06 10:36:27 -0500

    Fix: websocker url

--- config.ts
+++ config.ts
@@ -1,11 +1,11 @@
 export const Services = {
     //BaseUrl: "http://localhost:8081",
-    BaseUrl: import.meta.env.VITE_BASE_URL || "http://200.16.7.178/api",
+    BaseUrl: import.meta.env.VITE_BASE_URL || "https://1inf54-982-3c.inf.pucp.edu.pe/api",
     //BaseUrl:"https://localhost:44369",
     //BaseUrl:"http://54.235.188.31",
     //https://api.daoch.me
     //WebUrl: "ws://localhost:8081",
-    WebUrl: import.meta.env.VITE_WEB_URL || "wss://200.16.7.178/api",
+    WebUrl: import.meta.env.VITE_WEB_URL || "wss://1inf54-982-3c.inf.pucp.edu.pe/api",
     Headers: {
         'Content-Type': 'application/json'
     },
