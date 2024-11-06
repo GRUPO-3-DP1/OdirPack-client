942d53be7eda5235a81c7ee5c93ae9e93de96acb
maria <maria.rojasc@pucp.edu.pe>
2024-11-06 14:51:55 -0500

    mapa dia a a dia

--- src/pages/OperacionesDiaDia/Page.tsx
+++ src/pages/OperacionesDiaDia/Page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './page.module.css';
+import BaseMap from '../../components/Mapa/components/Mapa/MapaGeneral/BaseMap';
 
 const Page: React.FC = () => {
   return (
     <div className={styles.contenedor}>
-
+      <BaseMap />
     </div>
   );
 };
