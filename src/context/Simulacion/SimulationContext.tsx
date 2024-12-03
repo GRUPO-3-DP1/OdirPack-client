fc4ff8979f5b9c78801b222b8339395f9b12c350
maria5cisneros <m.l.cisneros.pazos@gmail.com>
2024-12-02 23:55:34 -0500

    cambios para simulacion

--- src/context/Simulacion/SimulationContext.tsx
+++ src/context/Simulacion/SimulationContext.tsx
@@ -44,18 +44,8 @@ function simulationReducer(state: SimulationState, action: SimulationAction): Si
       return { ...state, currentTime: action.payload };
     case 'SET_VEHICLES':
       return { ...state, vehicles: action.payload };
-    // case 'UPDATE_SIMULATION_DATA':
-    //   return { ...state, ...action.payload };
-    case 'SET_TOTAL_TRUCKS':
-      return { ...state, totalTrucks: action.payload };
-    case 'SET_OCCUPIED_OFFICES':
-      return { ...state, occupiedOffices: action.payload };
-    case 'SET_TRUCKS_IN_MOTION':
-      return { ...state, trucksInMotion: action.payload };
-    case 'SET_ORDERS_DELIVERED':
-      return { ...state, ordersDelivered: action.payload };
-    case 'SET_ORDERS_PENDING':
-      return { ...state, ordersPending: action.payload };
+    case 'UPDATE_SIMULATION_DATA':
+      return { ...state, ...action.payload };
     case 'SET_OFFICES':
       return { ...state, offices: action.payload };
     case 'SET_UNPLANNED_ORDERS':
@@ -162,20 +152,7 @@ export function SimulationProvider({ children }: { children: React.ReactNode; })
           //console.log('Primera respuesta:', newVehicles);
 
           // Actualizar datos de simulación
-          // dispatch({
-          //   type: 'UPDATE_SIMULATION_DATA',
-          //   payload: {
-          //     totalTrucks: newVehicles.length,
-          //     occupiedOffices: calculateOccupiedOffices(newOffices),
-          //   },
-          // });
-
-          // Actualizar 'totalTrucks'
-          dispatch({ type: 'SET_TOTAL_TRUCKS', payload: newVehicles.length });
-          // Actualizar 'occupiedOffices'
-          dispatch({ type: 'SET_OCCUPIED_OFFICES', payload: calculateOccupiedOffices(newOffices) });
-
-
+          
         } else {
           //console.log('Procesando');
 
@@ -219,20 +196,7 @@ export function SimulationProvider({ children }: { children: React.ReactNode; })
           dispatch({ type: 'SET_VEHICLES', payload: [...updatedVehicles] });
           //console.log('Vehículos actualizados:', updatedVehicles);
 
-          // Actualizar datos de simulación
-          // dispatch({
-          //   type: 'UPDATE_SIMULATION_DATA',
-          //   payload: {
-          //     totalTrucks: updatedVehicles.length,
-          //     occupiedOffices: calculateOccupiedOffices(newOffices),
-          //   },
-          // });
-
-          // Actualizar 'totalTrucks'
-          dispatch({ type: 'SET_TOTAL_TRUCKS', payload: updatedVehicles.length });
-          // Actualizar 'occupiedOffices'
-          dispatch({ type: 'SET_OCCUPIED_OFFICES', payload: calculateOccupiedOffices(newOffices) });
-
+      
         }
 
         // Actualizar oficinas en el estado
@@ -250,15 +214,7 @@ export function SimulationProvider({ children }: { children: React.ReactNode; })
   }, [indexActualProcess, solutions, lastProcessedSolution, state.vehicles, dispatch, state.offices]);
 
   // Función para calcular oficinas ocupadas
-  const calculateOccupiedOffices = (offices: Oficina[]): number => {
-    let occupied = 0;
-    offices.forEach((office) => {
-      if ((office.horasStock ?? []).some((horaStock) => horaStock.stock > 0)) {
-        occupied += 1;
-      }
-    });
-    return occupied;
-  };
+
 
   const timeIncrement = 1000;// Avanzar un segundo de simulación por intervalo
 
@@ -284,22 +240,7 @@ export function SimulationProvider({ children }: { children: React.ReactNode; })
         const endTime = new Date(ruta.fechasLlegada[ruta.fechasLlegada.length - 1]);
 
         // Verificar si el vehículo está en mantenimiento
-        if (vehicle.maintenance && vehicle.maintenance.inMaintenance) {
-          const maintenanceEndTime = new Date(
-            vehicle.maintenance.startTime.getTime() + vehicle.maintenance.duration
-          );
-          if (newTime >= maintenanceEndTime) {
-            // Finaliza el mantenimiento
-            vehicle = {
-              ...vehicle,
-              maintenance: undefined, // Eliminar el objeto maintenance
-            };
-            // Continuar para actualizar la posición
-          } else {
-            // Mantener el vehículo en mantenimiento
-            return vehicle;
-          }
-        }
+        
 
         // Detectar si el vehículo ha llegado a una oficina
         const arrivalTimes = ruta.fechasLlegada.map((fecha) => new Date(fecha));
@@ -464,23 +405,14 @@ export function SimulationProvider({ children }: { children: React.ReactNode; })
       dispatch({ type: 'SET_PROCESSED_ORDER_IDS', payload: newProcessedOrderIds });
 
       // Actualizar datos de simulación
-      // dispatch({
-      //   type: 'UPDATE_SIMULATION_DATA',
-      //   payload: {
-      //     trucksInMotion: calculateTrucksInMotion(updatedVehicles),
-      //     ordersDelivered: calculateOrdersDelivered(updatedVehicles, newTime),
-      //     ordersPending: calculateOrdersPending(updatedVehicles, newTime),
-      //   },
-      // });
-
-      // Actualizar 'trucksInMotion'
-      dispatch({ type: 'SET_TRUCKS_IN_MOTION', payload: calculateTrucksInMotion(updatedVehicles) });
-      // Actualizar 'ordersDelivered'
-      dispatch({ type: 'SET_ORDERS_DELIVERED', payload: calculateOrdersDelivered(updatedVehicles, newTime) });
-      // Actualizar 'ordersPending'
-      dispatch({ type: 'SET_ORDERS_PENDING', payload: calculateOrdersPending(updatedVehicles, newTime) });
-
-
+      dispatch({
+        type: 'UPDATE_SIMULATION_DATA',
+        payload: {
+          trucksInMotion: calculateTrucksInMotion(updatedVehicles),
+          ordersDelivered: calculateOrdersDelivered(updatedVehicles, newTime),
+          ordersPending: calculateOrdersPending(updatedVehicles, newTime),
+        },
+      });
     }, timeIncrement / state.speed);
 
     return () => clearInterval(updateInterval);
