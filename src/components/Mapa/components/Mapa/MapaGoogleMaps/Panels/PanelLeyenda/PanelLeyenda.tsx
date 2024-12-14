3a40085e9bad47806fdfc282710011557e8689ae
Andre <andre.saras@pucp.edu.pe>
2024-12-14 16:50:55 -0500

    Fix: changing the style of the legend panel with the new styles

--- src/components/Mapa/components/Mapa/MapaGoogleMaps/Panels/PanelLeyenda/PanelLeyenda.tsx
+++ src/components/Mapa/components/Mapa/MapaGoogleMaps/Panels/PanelLeyenda/PanelLeyenda.tsx
@@ -1,66 +1,55 @@
 import React, { useState } from 'react';
-import PanelBase from '../PanelBase/PanelBase';
-import { ControlPosition } from '@vis.gl/react-google-maps';
-import styles from './PanelLeyenda.module.css';
-import { IconButton, Switch } from '@mui/material';
-import { Close, Map } from '@mui/icons-material';
+import { Switch, Divider } from '@mui/material';
+import { Map } from '@mui/icons-material';
 import { leyendaItems } from '../../../../../../../data/leyendaItems';
 import { useMapMarker } from '../../../../../../../context/MapMarker/useMapMarker';
-import { Divider } from '@mui/material';
+import styles from './PanelLeyenda.module.css';
 
 type PanelLeyendaProps = {
   show?: boolean;
 };
 
 const PanelLeyenda: React.FC<PanelLeyendaProps> = ({ show = true }) => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   const { visibility, toggleVisibility } = useMapMarker();
+
+  if (!show) {
+    return null;
+  }
 
   return (
     <>
-      {
-        isOpen ?
-          <PanelBase show={show} position={ControlPosition.BOTTOM_LEFT} >
-            <div className={styles.container}>
-              <div className={styles.title}>
-                Leyenda
-                <IconButton
-                  onClick={() => setIsOpen(false)}
-                  size='small'
-                >
-                  <Close fontSize='inherit' />
-                </IconButton>
-              </div>
+      {/* Botón flotante para mostrar/ocultar la leyenda */}
+      <button
+        className={styles.floatingButton}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <Map />
+      </button>
 
-              <Divider sx={{ marginY: 0 }} />
-
-              <ul className={styles.lista}>
-                {leyendaItems.map((item, index) =>
-                  <li key={index} className={styles.item}>
-                    <Switch size='small' checked={visibility[item.name]} onClick={() => toggleVisibility(item.name)} />
-                    <div>
-                      {item.icon}
-                    </div>
-                    <span>{item.text}</span>
-                  </li>
-                )}
-              </ul>
+      {isOpen && (
+        <div className={styles.panelContainer}>
+          <div className={styles.container}>
+            <div className={styles.title}>
+              Leyenda
             </div>
-          </PanelBase >
-          :
-          <PanelBase
-            show={show}
-            position={ControlPosition.BOTTOM_LEFT}
-            className={styles.button}
-          >
-            <IconButton
-              onClick={() => setIsOpen(true)}
-              size='small'
-            >
-              <Map />
-            </IconButton>
-          </PanelBase >
-      }
+
+            <Divider sx={{ marginY: 0 }} />
+
+            <ul className={styles.lista}>
+              {leyendaItems.map((item, index) => (
+                <li key={index} className={styles.item}>
+                  <Switch size='small' checked={visibility[item.name]} onClick={() => toggleVisibility(item.name)} />
+                  <div>
+                    {item.icon}
+                  </div>
+                  <span>{item.text}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </>
   );
 };
