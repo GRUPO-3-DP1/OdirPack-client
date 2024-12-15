5ab32ef6ba1e6f1d6c3e323ac7797551aef24809
Diego Ochoa <88157175+daoch@users.noreply.github.com>
2024-12-14 23:53:23 -0500

    Update SimulationContext.tsx

--- src/context/Simulacion/SimulationContext.tsx
+++ src/context/Simulacion/SimulationContext.tsx
@@ -114,7 +114,7 @@ const initialOffices = oficinas.map((office) => ({
 const initialState: SimulationState = {
   isPlaying: false,
   //
-  speed: 18, //9: 1min = 1hora //25
+  speed: 9, //9: 1min = 1hora //25
   startTime: new Date('2024-10-01T00:00:00'),
   currentTime: new Date('2024-10-01T00:00:00'),
   endTime: new Date('2024-10-08T00:00:00'),
