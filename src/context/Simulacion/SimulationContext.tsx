03d298579489cebf92ad76a37b3aa7a18ab0c46e
Diego Ochoa <88157175+daoch@users.noreply.github.com>
2024-12-15 07:23:47 -0500

    Update SimulationContext.tsx

--- src/context/Simulacion/SimulationContext.tsx
+++ src/context/Simulacion/SimulationContext.tsx
@@ -114,7 +114,7 @@ function simulationReducer(state: SimulationState, action: SimulationAction): Si
 const initialState: SimulationState = {
   isPlaying: false,
   //
-  speed: 10, //9: 1min = 1hora //25
+  speed: 18, //9: 1min = 1hora //25
   startTime: new Date('2024-10-01T00:00:00'),
   currentTime: new Date('2024-10-01T00:00:00'),
   endTime: new Date('2024-10-08T00:00:00'),
